94d20eb92fa9eb7f590f6be2a04b249f35bc4ae6
ihnkyo.chung <jamanvo@naver.com>
2021-09-24 18:27:05 +0900

    기본 모델 추가

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=64, db_index=True)
+    released_at = models.DateField(null=True)
+    image_url = models.TextField(null=True)
+
+
+class Score(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.FloatField(default=0.0)
+
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
