ca703c41d19920de0e286837b7fd1dbecc61b463
jarvis <jarvis@jarvisui-MacBookPro.local>
2021-09-25 16:10:18 +0900

    Review 모델 수정

--- movies/models.py
+++ movies/models.py
@@ -8,13 +8,8 @@ class Movie(models.Model):
     image_url = models.TextField(null=True)
 
 
-class Score(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.FloatField(default=0.0)
-
-
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.FloatField(default=0.0)
     content = models.TextField()
